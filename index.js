bf00acc1fe88b033e0587020408f89360bcab0f9
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-03-27 22:59:43 +0300

    Modified Indec

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+function calculateTax(amount) {
+    return amount * 0.1;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2)
+}
+
+function isPalindrome(word){
+    const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    if ( cleanedWord == reversedWord) {
+        return true;
+    }
+    else;    
+        return false;
+}
+
+function calculateDiscountedPrice(originalPrice, discountpercentage) {
+    return originalPrice - (originalPrice * (discountpercentage / 100));
+}
 
 
 
